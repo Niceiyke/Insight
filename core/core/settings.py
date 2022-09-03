456668e294be0fef7d9c13c67955dd66c75ccd54
Niceiyke <oyomworld@gmail.com>
2022-09-03 08:21:45 +0100

    azura

--- core/core/settings.py
+++ core/core/settings.py
@@ -2,8 +2,7 @@
 import os
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
-import django_heroku
-import dj_database_url
+
 
 key=get_random_secret_key()
 print(key)
@@ -22,7 +21,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','x9dwz*l1@3b%n20uv035ks^4x#m37al-0=ve!p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG'))=='1'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://yinsight.azurewebsites.net']
 
 if not DEBUG:
     ALLOWED_HOSTS=[os.environ.get('ALLOWED_HOST')]
@@ -45,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,12 +137,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static'),
-]
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticfielsStorage'
+STATIC_ROOT = BASE_DIR/ 'static'
 
-django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
